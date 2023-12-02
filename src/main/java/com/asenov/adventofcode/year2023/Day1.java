3d0c965c14efb52a9d3de594867c4304e8a53163
Yordan Asenov <yordan.asenov.ext@siemens.com>
2023-12-02 02:26:05 +0100

    feat: day1 second part

--- src/main/java/com/asenov/adventofcode/year2023/Day1.java
+++ src/main/java/com/asenov/adventofcode/year2023/Day1.java
@@ -1,27 +1,101 @@
 package com.asenov.adventofcode.year2023;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 
 public class Day1 {
-    public static Integer firstNumber(String input) {
-        for (char c : input.toCharArray()) {
-            if (Character.isDigit(c)) {
-                return Integer.parseInt("" + c);
+    private final static Map<String, Integer> letterDigits = Map.of(
+        "one", 1,
+        "two", 2,
+        "three", 3,
+        "four", 4,
+        "five", 5,
+        "six", 6,
+        "seven", 7,
+        "eight", 8,
+        "nine", 9
+    );
+
+    public static Integer getIndexOfFirstNumberDigit(String input) {
+        int index = 999;
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))) {
+                index = i;
+                break;
             }
         }
-        return -1;
+        return index;
     }
 
-    public static String reverseString(String input) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(input);
-        sb.reverse();
-        return sb.toString();
+    public static Integer getIndexOfLastNumberDigit(String input) {
+        int index = -1;
+        for (int i = input.length() - 1; i > -1; i--) {
+            if (Character.isDigit(input.charAt(i))) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    public static Integer getIndexOfFirstLetterDigit(String input) {
+        return letterDigits.keySet().stream()
+            .map(input::indexOf)
+            .filter(index -> index > -1)
+            .min(Comparator.naturalOrder())
+            .orElse(99999);
+    }
+
+    public static Integer getIndexOfLastLetterDigit(String input) {
+        return letterDigits.keySet().stream()
+            .map(input::lastIndexOf)
+            .filter(index -> index > -1)
+            .max(Comparator.naturalOrder())
+            .orElse(-1);
+    }
+
+    public static Integer getFirstLetterDigit(String input, int index) {
+        return letterDigits.entrySet().stream()
+            .filter(digit -> input.indexOf(digit.getKey()) == index)
+            .map(Map.Entry::getValue)
+            .findFirst()
+            .orElse(-1);
+    }
+
+    public static Integer getLastLetterDigit(String input, int index) {
+        return letterDigits.entrySet().stream()
+            .filter(digit -> input.lastIndexOf(digit.getKey()) == index)
+            .map(Map.Entry::getValue)
+            .findFirst()
+            .orElse(-1);
+    }
+
+    public static Integer firstNumber(String input) {
+        Integer indexOfFirstLetterDigit = getIndexOfFirstLetterDigit(input);
+        Integer indexOfFirstNumberDigit = getIndexOfFirstNumberDigit(input);
+
+        if (indexOfFirstNumberDigit < indexOfFirstLetterDigit) {
+            return Integer.parseInt("" + input.charAt(indexOfFirstNumberDigit));
+        } else {
+            return getFirstLetterDigit(input, indexOfFirstLetterDigit);
+        }
+    }
+
+    public static Integer lastNumber(String input) {
+        Integer indexOfLastLetterDigit = getIndexOfLastLetterDigit(input);
+        Integer indexOfLastNumberDigit = getIndexOfLastNumberDigit(input);
+
+        if (indexOfLastNumberDigit > indexOfLastLetterDigit) {
+            return Integer.parseInt("" + input.charAt(indexOfLastNumberDigit));
+        } else {
+            return getLastLetterDigit(input, indexOfLastLetterDigit);
+        }
     }
 
     public static Integer combineFirstAndLastNumber(String input) {
         Integer firstNumber = firstNumber(input);
-        Integer lastNumber = firstNumber(reverseString(input));
+        Integer lastNumber = lastNumber(input);
         return Integer.parseInt(firstNumber + "" + lastNumber);
     }
 
--- src/test/java/com/asenov/adventofcode/year2023/Day1Test.java
+++ src/test/java/com/asenov/adventofcode/year2023/Day1Test.java
@@ -1,5 +1,6 @@
 package com.asenov.adventofcode.year2023;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -12,29 +13,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day1Test {
     @Test
     void firstNumber() {
-        String input = "1abc2";
+        String input = "5blgzg";
         Integer result = Day1.firstNumber(input);
-        assertEquals(1, result);
+        assertEquals(5, result);
     }
 
     @Test
     void lastNumber() {
-        String input = "1abc2";
-        Integer result = Day1.firstNumber(Day1.reverseString(input));
-        assertEquals(2, result);
+        String input = "5blgzg";
+        Integer result = Day1.lastNumber(input);
+        assertEquals(5, result);
     }
 
     @Test
     void combinationOfFirstAndLastNumber() {
-        String input = "1abc2";
+        String input = "5blgzg";
         Integer result = Day1.combineFirstAndLastNumber(input);
-        assertEquals(12, result);
+        assertEquals(55, result);
     }
 
     @Test
     void sumCalibrationValues() throws IOException {
-        List<String> inputs = Files.readAllLines(Path.of("src/test/resources/input/day1-example.txt"));
+        List<String> inputs = Files.readAllLines(Path.of("src/test/resources/input/day1-second-puzzle.txt"));
         Integer result = Day1.sumCalibrationValues(inputs);
-        assertEquals(142, result);
+        assertEquals(281, result);
     }
 }
