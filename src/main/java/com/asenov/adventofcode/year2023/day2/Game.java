00b4658e2bc573ea61c022090f444672690b9cf9
Yordan Asenov <yordan.asenov.ext@siemens.com>
2023-12-02 15:40:33 +0100

    feat: day2

---
+++ src/main/java/com/asenov/adventofcode/year2023/day2/Game.java
@@ -0,0 +1,13 @@
+package com.asenov.adventofcode.year2023.day2;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class Game {
+    private Integer id;
+    private List<GameConfiguration> configurations;
+}
