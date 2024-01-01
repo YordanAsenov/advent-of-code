e58de65b11a92a4d7cac80454f7f6a163da05a78
Yordan Asenov <yordanasenov@gmail.com>
2024-01-01 01:57:36 +0100

    feat: day14 - first part

--- src/test/java/com/asenov/adventofcode/year2023/Day14Test.java
+++ src/test/java/com/asenov/adventofcode/year2023/Day14Test.java
@@ -1,5 +1,6 @@
 package com.asenov.adventofcode.year2023;
 
+import com.asenov.adventofcode.utils.ListUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,20 +8,62 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class Day14Test {
     @Test
     void solveFirstPartExample() throws IOException {
-        List<String> input = Files.readAllLines(Path.of("src/test/resources/input/day14-example.txt"));
+        List<String> rows = Files.readAllLines(Path.of("src/test/resources/input/day14-example.txt"));
+        List<String> columns = ListUtils.transpose(rows);
 
-        assertTrue(true);
+        int count = 0;
+        for (String column : columns) {
+            char[] array = column.toCharArray();
+            for (int j = 0; j < array.length; j++) {
+                for (int k = 0; k < array.length - 1; k++) {
+                    if (array[k] == '.' && array[k + 1] == 'O') {
+                        array[k + 1] = '.';
+                        array[k] = 'O';
+                    }
+                }
+            }
+
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == 'O') {
+                    count += (array.length - j);
+                }
+            }
+        }
+
+        assertEquals(136, count);
     }
 
     @Test
     void solveFirstPartPuzzle() throws IOException {
-        // List<String> input = Files.readAllLines(Path.of("src/test/resources/input/day14-puzzle.txt"));
-        assertTrue(true);
+        List<String> rows = Files.readAllLines(Path.of("src/test/resources/input/day14-puzzle.txt"));
+        List<String> columns = ListUtils.transpose(rows);
+
+        int count = 0;
+        for (String column : columns) {
+            char[] array = column.toCharArray();
+            for (int j = 0; j < array.length; j++) {
+                for (int k = 0; k < array.length - 1; k++) {
+                    if (array[k] == '.' && array[k + 1] == 'O') {
+                        array[k + 1] = '.';
+                        array[k] = 'O';
+                    }
+                }
+            }
+
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == 'O') {
+                    count += (array.length - j);
+                }
+            }
+        }
+
+        assertEquals(108144, count);
     }
 
     @Test
