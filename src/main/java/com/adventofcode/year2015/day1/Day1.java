0359940a9e948075ad02c1a5c66eaf03c2363c4c
Yordan Asenov <yordanasenov@gmail.com>
2024-10-05 16:09:21 +0200

    chore: solve day 1 of 2015

--- src/main/java/com/adventofcode/year2015/day1/Day1.java
+++ src/main/java/com/adventofcode/year2015/day1/Day1.java
@@ -1,5 +1,29 @@
 package com.adventofcode.year2015.day1;
 
+import java.util.List;
+
 public class Day1 {
-    
+
+    public static int solve(List<String> input, boolean shouldExitAtMinusOne) {
+        int count = 0;
+        int position = 0;
+
+        for (String row : input) {
+            char[] commands = row.toCharArray();
+            for (int i = 0; i < commands.length; i++) {
+                position++;
+
+                char command = commands[i];
+                if (command == '(')
+                    count++;
+                if (command == ')')
+                    count--;
+
+                if (shouldExitAtMinusOne && count == -1)
+                    return position;
+            }
+        }
+
+        return count;
+    }
 }
--- src/test/java/com/adventofcode/year2015/Day1Test.java
+++ src/test/java/com/adventofcode/year2015/Day1Test.java
@@ -1,5 +1,6 @@
 package com.adventofcode.year2015;
 
+import com.adventofcode.year2015.day1.Day1;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,30 +8,36 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class Day1Test {
+
     @Test
     void solveFirstPartExample() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2015/day1/first-example.txt"));
-        assertNotNull(input);
+        int result = Day1.solve(input, false);
+        assertEquals(3, result);
     }
 
     @Test
     void solveFirstPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2015/day1/first-puzzle.txt"));
-        assertNotNull(input);
+        int result = Day1.solve(input, false);
+        assertEquals(138, result);
     }
 
     @Test
     void solveSecondPartExample() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2015/day1/second-example.txt"));
-        assertNotNull(input);
+        int result = Day1.solve(input, true);
+        assertEquals(5, result);
     }
 
     @Test
     void solveSecondPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2015/day1/second-puzzle.txt"));
-        assertNotNull(input);
+        int result = Day1.solve(input, true);
+        assertEquals(1771, result);
     }
 }
