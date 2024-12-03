7d39cbf466a54be5b0a253516439ed15e3ce5a7c
Yordan Asenov <yordanasenov@gmail.com>
2024-12-03 23:48:34 +0100

    chore: solve day 3 of 2024

--- src/main/java/com/adventofcode/year2024/day3/Day3.java
+++ src/main/java/com/adventofcode/year2024/day3/Day3.java
@@ -1,5 +1,111 @@
 package com.adventofcode.year2024.day3;
 
+import java.util.List;
+
 public class Day3 {
-    
+
+    public static boolean allDigits(String input) {
+        if (input.isEmpty() || input.contains(" ")) {
+            return false;
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+
+        int test = Integer.parseInt(input);
+        return test >= 0 && test <= 999;
+    }
+
+    public static int solve(List<String> input) {
+        int sum = 0;
+
+        for (String sequence : input) {
+            while (!sequence.isEmpty()) {
+                int start = sequence.indexOf("mul(");
+                int firstValueIndex = start + 4;
+                int end = sequence.indexOf(")", firstValueIndex);
+                if (start == -1 || end == -1) {
+                    break;
+                }
+
+                String section = sequence.substring(firstValueIndex, end);
+                if (section.contains("mul(")) {
+                    sequence = sequence.substring(firstValueIndex);
+                    continue;
+                }
+
+                String[] numbers = section.split(",");
+                if (numbers.length > 2 || !allDigits(numbers[0]) || !allDigits(numbers[1])) {
+                    sequence = sequence.substring(firstValueIndex);
+                    continue;
+                }
+
+                Integer first = Integer.parseInt(numbers[0]);
+                Integer second = Integer.parseInt(numbers[1]);
+                sum += (first * second);
+
+                sequence = sequence.substring(end + 1);
+            }
+        }
+
+        return sum;
+    }
+
+    public static int solve2(List<String> input) {
+        int sum = 0;
+
+        boolean shouldDo = true;
+
+        for (String sequence : input) {
+            String initialSequence = sequence;
+            int index;
+
+            while (!sequence.isEmpty()) {
+                int start = sequence.indexOf("mul(");
+                int firstValueIndex = start + 4;
+                int end = sequence.indexOf(")", firstValueIndex);
+                if (start == -1 || end == -1) {
+                    break;
+                }
+
+                String section = sequence.substring(firstValueIndex, end);
+                if (section.contains("mul(")) {
+                    sequence = sequence.substring(firstValueIndex);
+                    continue;
+                }
+
+                String[] numbers = section.split(",");
+                if (numbers.length > 2 || !allDigits(numbers[0]) || !allDigits(numbers[1])) {
+                    sequence = sequence.substring(firstValueIndex);
+                    continue;
+                }
+
+                String operation = sequence.substring(start, end + 1);
+                index = (initialSequence.lastIndexOf(operation));
+
+                String lastCommand = initialSequence.substring(0, index);
+                int lastDoIndex = lastCommand.lastIndexOf("do()");
+                int lastDontIndex = lastCommand.lastIndexOf("don't()");
+
+                if (lastDoIndex > lastDontIndex) {
+                    shouldDo = true;
+                } else if (lastDontIndex > lastDoIndex ) {
+                    shouldDo = false;
+                }
+
+                if (shouldDo) {
+                    Integer first = Integer.parseInt(numbers[0]);
+                    Integer second = Integer.parseInt(numbers[1]);
+                    sum += (first * second);
+                }
+
+                sequence = sequence.substring(end + 1);
+            }
+        }
+
+        return sum;
+    }
 }
--- src/test/java/com/adventofcode/year2024/Day3Test.java
+++ src/test/java/com/adventofcode/year2024/Day3Test.java
@@ -1,5 +1,6 @@
 package com.adventofcode.year2024;
 
+import com.adventofcode.year2024.day3.Day3;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,30 +8,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day3Test {
     @Test
     void solveFirstPartExample() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day3/first-example.txt"));
-        assertNotNull(input);
+        int result = Day3.solve(input);
+        assertEquals(161, result);
     }
 
     @Test
     void solveFirstPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day3/first-puzzle.txt"));
-        assertNotNull(input);
+        int result = Day3.solve(input);
+        assertEquals(2, result);
     }
 
     @Test
     void solveSecondPartExample() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day3/second-example.txt"));
-        assertNotNull(input);
+        long result = Day3.solve2(input);
+        assertEquals(48, result);
     }
 
     @Test
     void solveSecondPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day3/second-puzzle.txt"));
-        assertNotNull(input);
+        int result = Day3.solve2(input);
+        assertEquals(71668682, result);
     }
 }
