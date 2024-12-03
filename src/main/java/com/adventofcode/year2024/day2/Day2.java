020d02320a742962219ce82798348a5b479a08e6
Yordan Asenov <yordanasenov@gmail.com>
2024-12-03 01:02:13 +0100

    chore: solve day 2 of 2024

--- src/main/java/com/adventofcode/year2024/day2/Day2.java
+++ src/main/java/com/adventofcode/year2024/day2/Day2.java
@@ -1,5 +1,82 @@
 package com.adventofcode.year2024.day2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Day2 {
-    
+
+    private static boolean isSafe(List<Integer> levels) {
+        final int MIN_DIFF = 1;
+        final int MAX_DIFF = 3;
+
+        int steps = levels.size() - 1;
+
+        int countIncreasing = 0;
+        int countDecreasing = 0;
+        int allowedDiffs = 0;
+
+        for (int i = 0; i < steps; i++) {
+            int currentLevel = levels.get(i);
+            int nextLevel = levels.get(i + 1);
+
+            boolean isGrowing = currentLevel < nextLevel;
+            if (isGrowing) {
+                countIncreasing++;
+            }
+            if (!isGrowing) {
+                countDecreasing++;
+            }
+
+            int diff = isGrowing ? nextLevel - currentLevel : currentLevel - nextLevel;
+            if (diff >= MIN_DIFF && diff <= MAX_DIFF) {
+                allowedDiffs++;
+            }
+        }
+
+        if (countIncreasing != steps && countDecreasing != steps) {
+            return false;
+        }
+
+        return allowedDiffs == steps;
+    }
+
+    private static int countSafe(List<String> input, int toleration) {
+        int result = 0;
+
+        for (String report : input) {
+            List<Integer> levels = Arrays.stream(report.split(" "))
+                .map(Integer::parseInt)
+                .toList();
+
+            boolean isReportSafe = isSafe(levels);
+            if (isReportSafe) {
+                result++;
+                System.out.println("Report is safe: " + report);
+            } else {
+                if (toleration > 0) {
+                    for (int i = 0; i < levels.size(); i++) {
+                        ArrayList<Integer> temp = new ArrayList<>(levels);
+                        temp.remove(i);
+
+                        if (isSafe(temp)) {
+                            System.out.println("Report is safe: " + report);
+                            result++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static int solve(List<String> input) {
+        return countSafe(input, 0);
+    }
+
+    public static int solve2(List<String> input) {
+        return countSafe(input, 1);
+    }
 }
--- src/test/java/com/adventofcode/year2024/Day2Test.java
+++ src/test/java/com/adventofcode/year2024/Day2Test.java
@@ -1,5 +1,6 @@
 package com.adventofcode.year2024;
 
+import com.adventofcode.year2024.day2.Day2;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,30 +8,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day2Test {
     @Test
     void solveFirstPartExample() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day2/first-example.txt"));
-        assertNotNull(input);
+        int result = Day2.solve(input);
+        assertEquals(2, result);
     }
 
     @Test
     void solveFirstPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day2/first-puzzle.txt"));
-        assertNotNull(input);
+        int result = Day2.solve(input);
+        assertEquals(663, result);
     }
 
     @Test
     void solveSecondPartExample() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day2/second-example.txt"));
-        assertNotNull(input);
+        int result = Day2.solve2(input);
+        assertEquals(4, result);
     }
 
     @Test
     void solveSecondPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day2/second-puzzle.txt"));
-        assertNotNull(input);
+        int result = Day2.solve2(input);
+        assertEquals(692, result);
     }
 }
