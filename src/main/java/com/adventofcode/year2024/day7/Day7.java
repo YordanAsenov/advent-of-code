c965d673602a63b14bb13676a36281b1617b6981
Yordan Asenov <yordanasenov@gmail.com>
2024-12-07 13:28:53 +0100

    chore: solve part 2 of day 7 of 2024

--- src/main/java/com/adventofcode/year2024/day7/Day7.java
+++ src/main/java/com/adventofcode/year2024/day7/Day7.java
@@ -13,31 +13,66 @@ public class Day7 {
     @Data
     private static class Equation {
         private long testValue;
-        private List<Integer> numbers;
+        private List<Long> numbers;
 
         public boolean isValid() {
             return compute(this.testValue, numbers.getFirst(), numbers.subList(1, numbers.size())) != -1;
         }
 
         // return -1 if not found or the correct testValue
-        private long compute(long testValue, long partial, List<Integer> otherNumbers) {
+        private long compute(long testValue, long partial, List<Long> otherNumbers) {
             if (otherNumbers.isEmpty()) {
                 return -1;
             }
 
             long next = otherNumbers.getFirst();
-            if (otherNumbers.size() == 1 && (partial + next == testValue || partial * next == testValue)) {
+
+            long partialSum = partial + next;
+            long partialMultiplication = partial * next;
+
+            if (otherNumbers.size() == 1 && (partialSum == testValue || partialMultiplication == testValue)) {
                 return testValue;
             }
 
-            long newPartial = partial + next;
-            var t1 = compute(testValue, newPartial, otherNumbers.subList(1, otherNumbers.size()));
+            var t1 = compute(testValue, partialSum, otherNumbers.subList(1, otherNumbers.size()));
             if (t1 != -1) {
                 return t1;
             }
 
-            newPartial = partial * next;
-            return compute(testValue, newPartial, otherNumbers.subList(1, otherNumbers.size()));
+            return compute(testValue, partialMultiplication, otherNumbers.subList(1, otherNumbers.size()));
+        }
+
+        public boolean isValid2() {
+            return compute2(this.testValue, numbers.getFirst(), numbers.subList(1, numbers.size())) != -1;
+        }
+
+        // return -1 if not found or the correct testValue
+        private long compute2(Long testValue, Long partial, List<Long> otherNumbers) {
+            if (otherNumbers.isEmpty()) {
+                return -1;
+            }
+
+            Long next = otherNumbers.getFirst();
+
+            long partialSum = partial + next;
+            long partialMultiplication = partial * next;
+            long partialConcatenation = Long.parseLong(partial + "" + next);
+
+            if (otherNumbers.size() == 1 && (partialSum == testValue || partialMultiplication == testValue || partialConcatenation == testValue)) {
+                return testValue;
+            }
+
+            var t1 = compute2(testValue, partialSum, otherNumbers.subList(1, otherNumbers.size()));
+            if (t1 != -1) {
+                return t1;
+            }
+
+            var t2 = compute2(testValue, partialMultiplication, otherNumbers.subList(1, otherNumbers.size()));
+            if (t2 != -1) {
+                return t2;
+            }
+
+            return compute2(testValue, partialConcatenation, otherNumbers.subList(1, otherNumbers.size()));
         }
     }
 
@@ -48,7 +83,7 @@ public class Day7 {
             String[] split = row.split(":");
             equations.add(new Equation(
                 Long.parseLong(split[0]),
-                Arrays.stream(split[1].substring(1).split(" ")).map(Integer::parseInt).toList()
+                Arrays.stream(split[1].substring(1).split(" ")).map(Long::parseLong).toList()
             ));
         }
 
@@ -59,7 +94,21 @@ public class Day7 {
             .reduce(0L, Long::sum);
     }
 
-    public static int solve2(List<String> input) {
-        return 0;
+    public static long solve2(List<String> input) {
+        List<Equation> equations = new ArrayList<>();
+
+        for (String row : input) {
+            String[] split = row.split(":");
+            equations.add(new Equation(
+                Long.parseLong(split[0]),
+                Arrays.stream(split[1].substring(1).split(" ")).map(Long::parseLong).toList()
+            ));
+        }
+
+        return equations.stream()
+            .filter(Equation::isValid2)
+            .peek((e) -> System.out.println("The equation is valid: " + e.testValue))
+            .map(Equation::getTestValue)
+            .reduce(0L, Long::sum);
     }
 }
--- src/test/java/com/adventofcode/year2024/Day7Test.java
+++ src/test/java/com/adventofcode/year2024/Day7Test.java
@@ -29,13 +29,13 @@ class Day7Test {
     void solveSecondPartExample() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day7/second-example.txt"));
         long result = Day7.solve2(input);
-        assertEquals(0, result);
+        assertEquals(11387, result);
     }
 
     @Test
     void solveSecondPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day7/second-puzzle.txt"));
         long result = Day7.solve2(input);
-        assertEquals(0, result);
+        assertEquals(91377448644679L, result);
     }
 }
