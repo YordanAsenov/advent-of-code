9d041eb7e10065941c97ffd055ecc961b08de745
Yordan Asenov <yordanasenov@gmail.com>
2024-12-12 02:44:17 +0100

    chore: solve part 2 of day 11 of 2024

--- src/main/java/com/adventofcode/year2024/day11/Day11.java
+++ src/main/java/com/adventofcode/year2024/day11/Day11.java
@@ -1,16 +1,18 @@
 package com.adventofcode.year2024.day11;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Day11 {
 
-    private static final HashMap<BigInteger, BigInteger> partialResult = new HashMap<>();
+    private static final Map<Turn, BigInteger> firstLevelCache = new ConcurrentHashMap<>();
 
-    private static final HashMap<BigInteger, List<BigInteger>> inventory = new HashMap<>();
+    private static final Map<BigInteger, List<BigInteger>> secondLevelCache = new ConcurrentHashMap<>();
+
+    private static final Map<BigInteger, BigInteger> thirdLevelCache = new ConcurrentHashMap<>();
 
     private static List<BigInteger> compute(BigInteger stone) {
         if (BigInteger.ZERO.equals(stone)) {
@@ -20,34 +22,45 @@ public class Day11 {
             String secondPart = stone.toString().substring(stone.toString().length() / 2);
             return List.of(new BigInteger(firstPart), new BigInteger(secondPart));
         } else {
-            return List.of(partialResult.computeIfAbsent(stone, s -> s.multiply(BigInteger.valueOf(2024L))));
+            return List.of(thirdLevelCache.computeIfAbsent(stone, s -> s.multiply(BigInteger.valueOf(2024L))));
         }
     }
 
-    private static List<BigInteger> blink(List<BigInteger> stones) {
-        List<BigInteger> newStones = new ArrayList<>();
-        for (BigInteger stone : stones) {
-            newStones.addAll(inventory.computeIfAbsent(stone, Day11::compute));
+    private static BigInteger compute(BigInteger stone, int times) {
+        BigInteger sum = BigInteger.ZERO;
+        List<BigInteger> newStones = secondLevelCache.computeIfAbsent(stone, Day11::compute);
+        for (BigInteger newStone : newStones.reversed()) {
+            sum = sum.add(blink(newStone, times));
         }
-        return newStones;
+        return sum;
     }
 
-    private static List<BigInteger> blink(List<BigInteger> stones, int times) {
-        int count = 0;
-        while (count < times) {
-            System.out.println("Blink: " + count);
-            stones = blink(stones);
-            count++;
+    private record Turn(BigInteger stone, int times) {}
+
+    private static BigInteger blink(BigInteger stone, int times) {
+        if (times == 0) {
+            return BigInteger.ONE;
         }
 
-        return stones;
+        var turn = new Turn(stone, times - 1);
+        BigInteger result = firstLevelCache.get(turn);
+        if (result == null) {
+            result = compute(turn.stone, turn.times);
+            firstLevelCache.put(turn, result);
+        }
+
+        return result;
     }
 
-    public static long solve(String input, int times) {
+    public static BigInteger solve(String input, int times) {
         List<BigInteger> stones = Arrays.stream(input.split(" "))
             .map(BigInteger::new)
             .toList();
 
-        return blink(stones, times).size();
+        BigInteger sum = BigInteger.ZERO;
+        for (BigInteger stone : stones.reversed()) {
+            sum = sum.add(blink(stone, times));
+        }
+        return sum;
     }
 }
--- src/test/java/com/adventofcode/year2024/Day11Test.java
+++ src/test/java/com/adventofcode/year2024/Day11Test.java
@@ -3,34 +3,36 @@ package com.adventofcode.year2024;
 import com.adventofcode.year2024.day11.Day11;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day11Test {
     @Test
     void solveFirstPartExampleOne() {
         String input = "0 1 10 99 999";
-        long result = Day11.solve(input, 1);
-        assertEquals(7, result);
+        BigInteger result = Day11.solve(input, 1);
+        assertEquals(BigInteger.valueOf(7), result);
     }
 
     @Test
     void solveFirstPartExampleTwo() {
         String input = "125 17";
-        long result = Day11.solve(input, 25);
-        assertEquals(55312, result);
+        BigInteger result = Day11.solve(input, 25);
+        assertEquals(BigInteger.valueOf(55312), result);
     }
 
     @Test
     void solveFirstPartPuzzle() {
         String input = "70949 6183 4 3825336 613971 0 15 182";
-        long result = Day11.solve(input, 25);
-        assertEquals(185205, result);
+        BigInteger result = Day11.solve(input, 25);
+        assertEquals(BigInteger.valueOf(185205), result);
     }
 
     @Test
     void solveSecondPartPuzzle() {
         String input = "70949 6183 4 3825336 613971 0 15 182";
-        long result = Day11.solve(input, 75);
-        assertEquals(185205, result);
+        BigInteger result = Day11.solve(input, 75);
+        assertEquals(BigInteger.valueOf(221280540398419L), result);
     }
 }
