5d17c08a48fa2919277aefe9779b3614dd21849b
Yordan Asenov <yordanasenov@gmail.com>
2024-12-14 19:40:41 +0100

    chore: solve part 1 of day 14 of 2024

--- src/main/java/com/adventofcode/year2024/day14/Day14.java
+++ src/main/java/com/adventofcode/year2024/day14/Day14.java
@@ -1,10 +1,119 @@
 package com.adventofcode.year2024.day14;
 
+import com.adventofcode.utils.Position;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day14 {
-    public static int solve(List<String> input) {
-        return 0;
+
+    public record Robot(Position position, Position velocity) {
+        private Robot move() {
+            int x = position.getX() + velocity.getX();
+            int y = position.getY() + velocity.getY();
+            return new Robot(Position.of(x, y), velocity);
+        }
+
+        public Robot moveInside(Grid grid) {
+            var newRobot = move();
+            return new Robot(grid.constrain(newRobot.position), newRobot.velocity);
+        }
+    }
+
+    public record Grid(int width, int height) {
+        public Position constrain(Position position) {
+            int x = (position.getX() % width + width) % width;
+            int y = (position.getY() % height + height) % height;
+            return Position.of(x, y);
+        }
+
+        public List<Quadrant> getQuadrants() {
+            int xMiddle = width / 2;
+            int yMiddle = height / 2;
+            List<Integer> xPositions = List.of(0, xMiddle - 1, xMiddle + 1, width - 1);
+            List<Integer> yPositions = List.of(0, yMiddle - 1, yMiddle + 1, height - 1);
+
+            List<Quadrant> quadrants = new ArrayList<>();
+            quadrants.add(new Quadrant(
+                Position.of(xPositions.getFirst(), yPositions.getFirst()),
+                Position.of(xPositions.get(1), yPositions.get(1))
+            ));
+            quadrants.add(new Quadrant(
+                Position.of(xPositions.get(2), yPositions.getFirst()),
+                Position.of(xPositions.getLast(), yPositions.get(1))
+            ));
+            quadrants.add(new Quadrant(
+                Position.of(xPositions.getFirst(), yPositions.get(2)),
+                Position.of(xPositions.get(1), yPositions.getLast())
+            ));
+            quadrants.add(new Quadrant(
+                Position.of(xPositions.get(2), yPositions.get(2)),
+                Position.of(xPositions.getLast(), yPositions.getLast())
+            ));
+            return quadrants;
+        }
+    }
+
+    public record Quadrant(Position min, Position max) {}
+
+    private static Position init(String input) {
+        String[] parts = input.substring(2).split(",");
+        return new Position(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+    }
+
+    public static List<Robot> buildRobots(List<String> input) {
+        List<Robot> robots = new ArrayList<>();
+        for (String row : input) {
+            String[] parts = row.split(" ");
+            Position position = init(parts[0]);
+            Position velocity = init(parts[1]);
+            robots.add(new Robot(position, velocity));
+        }
+        return robots;
+    }
+
+    public static long countRobotsInQuadrant(List<Position> positions, Quadrant quadrant) {
+        return positions.stream()
+            .filter(r ->
+                (r.getX() >= quadrant.min.getX() &&
+                    r.getX() <= quadrant.max.getX()) &&
+                (r.getY() >= quadrant.min.getY() &&
+                    r.getY() <= quadrant.max.getY())
+            )
+            .count();
+    }
+
+    public static long getSecureFactor(List<Position> robotPositions, Grid grid) {
+        List<Quadrant> quadrants = grid.getQuadrants();
+
+        long robotsInFirstQuadrant = countRobotsInQuadrant(robotPositions, quadrants.get(0));
+        long robotsInSecondQuadrant = countRobotsInQuadrant(robotPositions, quadrants.get(1));
+        long robotsInThirdQuadrant = countRobotsInQuadrant(robotPositions, quadrants.get(2));
+        long robotsInFourthQuadrant = countRobotsInQuadrant(robotPositions, quadrants.get(3));
+
+        return robotsInFirstQuadrant * robotsInSecondQuadrant * robotsInThirdQuadrant * robotsInFourthQuadrant;
+    }
+
+    public static List<Position> moveRobotsInGrid(List<String> input, int times, Grid grid) {
+        List<Robot> robots = buildRobots(input);
+
+        for (int i = 0; i < times; i++) {
+            List<Robot> robotsAfterMove = new ArrayList<>();
+            for (Robot robot : robots) {
+                robot = robot.moveInside(grid);
+                robotsAfterMove.add(robot);
+            }
+            robots = robotsAfterMove;
+        }
+
+        return robots.stream()
+            .map(r -> r.position)
+            .toList();
+    }
+
+    public static long solve(List<String> input, int width, int height) {
+        Grid grid = new Grid(width, height);
+        List<Position> robotPositions = moveRobotsInGrid(input, 100, grid);
+        return getSecureFactor(robotPositions, grid);
     }
 
     public static int solve2(List<String> input) {
--- src/test/java/com/adventofcode/year2024/Day14Test.java
+++ src/test/java/com/adventofcode/year2024/Day14Test.java
@@ -1,35 +1,149 @@
 package com.adventofcode.year2024;
 
+import com.adventofcode.utils.Position;
 import com.adventofcode.year2024.day14.Day14;
+import com.adventofcode.year2024.day14.Day14.Grid;
+import com.adventofcode.year2024.day14.Day14.Quadrant;
+import com.adventofcode.year2024.day14.Day14.Robot;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Day14Test {
+
+    private static final List<String> sampleInput = """
+         p=0,4 v=3,-3
+         p=6,3 v=-1,-3
+         p=10,3 v=-1,2
+         p=2,0 v=2,-1
+         p=0,0 v=1,3
+         p=3,0 v=-2,-2
+         p=7,6 v=-1,-3
+         p=3,0 v=-1,-2
+         p=9,3 v=2,3
+         p=7,3 v=-1,2
+         p=2,4 v=2,-3
+         p=9,5 v=-3,-3""".lines().toList();
+
+    @Test
+    void robotExist() {
+        var position = Position.of(0, 0);
+        var velocity = Position.of(5, 6);
+        var robot = new Robot(position, velocity);
+
+        assertEquals(position, robot.position());
+        assertEquals(velocity, robot.velocity());
+    }
+
+    @Test
+    void parseInput() {
+        var input = "p=6,3 v=-1,-3";
+        var expectedRobot = new Robot(Position.of(6, 3), Position.of(-1, -3));
+
+        List<Robot> robots = Day14.buildRobots(List.of(input));
+
+        assertEquals(expectedRobot, robots.getFirst());
+    }
+
+    @Test
+    void testRobotMoves() {
+        List<Position> expectedPositions = new ArrayList<>();
+        expectedPositions.add(Position.of(6,0));
+        expectedPositions.add(Position.of(6,0));
+        expectedPositions.add(Position.of(9,0));
+        expectedPositions.add(Position.of(0,2));
+        expectedPositions.add(Position.of(1,3));
+        expectedPositions.add(Position.of(2,3));
+        expectedPositions.add(Position.of(5,4));
+        expectedPositions.add(Position.of(3,5));
+        expectedPositions.add(Position.of(4,5));
+        expectedPositions.add(Position.of(4,5));
+        expectedPositions.add(Position.of(1,6));
+        expectedPositions.add(Position.of(6,6));
+
+        Grid grid = new Grid(11, 7);
+        List<Position> robotsPositions = Day14.moveRobotsInGrid(sampleInput, 100, grid);
+
+        assertEquals(List.of(), expectedPositions.stream().filter(p -> !robotsPositions.contains(p)).toList());
+        assertEquals(List.of(), robotsPositions.stream().filter(p -> !expectedPositions.contains(p)).toList());
+    }
+
+    @Test
+    void robotMovesLikePacman() {
+        var grid = new Grid(10, 10);
+        var robot = new Robot(Position.of(6, 3), Position.of(-7, 10));
+        var expectedRobot = new Robot(Position.of(9, 3), Position.of(-7, 10));
+
+        var newRobot = robot.moveInside(grid);
+
+        assertEquals(expectedRobot, newRobot);
+    }
+
+    @Test
+    void gridProvidesQuadrants() {
+        var grid = new Grid(11, 7);
+        var expectedQuadrants = List.of(
+            new Quadrant(Position.of(0,0), Position.of(4,2)),
+            new Quadrant(Position.of(6,0), Position.of(10,2)),
+            new Quadrant(Position.of(0,4), Position.of(4,6)),
+            new Quadrant(Position.of(6,4), Position.of(10,6))
+        );
+
+        var quadrants = grid.getQuadrants();
+
+        assertEquals(expectedQuadrants, quadrants);
+    }
+
+    @Test
+    void countRobotsInQuadrant() {
+        var robotPositions = List.of(
+            Position.of(6, 0),
+            Position.of(6, 0),
+            Position.of(9, 0),
+            Position.of(0, 2),
+            Position.of(1, 3),
+            Position.of(2, 3),
+            Position.of(5, 4),
+            Position.of(3, 5),
+            Position.of(4, 5),
+            Position.of(4, 5),
+            Position.of(1, 6),
+            Position.of(6, 6)
+        );
+        var quadrant = new Quadrant(Position.of(0,0), Position.of(4,2));
+
+        long count = Day14.countRobotsInQuadrant(robotPositions, quadrant);
+
+        assertEquals(1, count);
+    }
+
+    @Test
+    void computeSafetyFactor() {
+        var grid = new Grid(11, 7);
+        List<Position> robotPositions = Day14.moveRobotsInGrid(sampleInput, 100, grid);
+
+        long secureFactor = Day14.getSecureFactor(robotPositions, grid);
+
+        assertEquals(12, secureFactor);
+    }
+
     @Test
     void solveFirstPartExample() throws IOException {
-        List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day14/first-example.txt"));
-        int result = Day14.solve(input);
-        assertEquals(0, result);
+        long result = Day14.solve(sampleInput, 11, 7);
+        assertEquals(12, result);
     }
 
     @Test
     void solveFirstPartPuzzle() throws IOException {
         List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day14/first-puzzle.txt"));
-        int result = Day14.solve(input);
-        assertEquals(0, result);
-    }
-
-    @Test
-    void solveSecondPartExample() throws IOException {
-        List<String> input = Files.readAllLines(Path.of("src/test/resources/input/2024/day14/second-example.txt"));
-        int result = Day14.solve2(input);
-        assertEquals(0, result);
+        long result = Day14.solve(input, 101, 103);
+        assertEquals(236628054, result);
     }
 
     @Test
